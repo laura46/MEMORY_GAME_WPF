7c7c208bb7c56eb5ab4862923e942da1eb181a82
Patrick Norden <petjenorden@gmail.com>
2019-10-22 20:31:37 +0200

    Ik heb de code aangepast zodat er plaatjes te zien zijn in de grid

--- MemoryGrid.cs
+++ MemoryGrid.cs
@@ -3,22 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace MemoryProject
 {
     class MemoryGrid
     {
         private Grid grid;
-        public RowDefinition row;
-        public ColumnDefinition col;
-
-        public void doGrid() { }
+        const int rows = 4;
+        const int cols = 4;
 
         public MemoryGrid(Grid grid, int cols, int rows)
         {
             this.grid = grid;
             InitializeGameGrid(cols, rows);
+            AddImages();
+        }
+
+        private void AddImages()
+        {
+            List<ImageSource> images = GetImagesList();
+            for(int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < cols; column++)
+                {
+                    Image backgroundImage = new Image();
+                    backgroundImage.Source = new BitmapImage(new Uri("Kaartjes/Achterkant.svg", UriKind.Relative));
+                    backgroundImage.MouseDown += new MouseButtonEventHandler(CardClick);
+                    Grid.SetColumn(backgroundImage, column);
+                    Grid.SetRow(backgroundImage, row);
+                    grid.Children.Add(backgroundImage);
+                }
+            }
+        }
+
+        private void CardClick(object sender, MouseButtonEventArgs e)
+        {
+            Image card = (Image)sender;
+            ImageSource front = (ImageSource)card.Tag;
+            card.Source = front;
+        }
+        private List<ImageSource> GetImagesList()
+        {
+            List<ImageSource> images = new List<ImageSource>();
+            for (int i = 0; i < 16; i++)
+            {
+                int imageNr = i % 8 + 1;
+                ImageSource source = new BitmapImage(new Uri("Kaartjes/" + imageNr + ".svg", UriKind.Relative));
+                images.Add(source);
+            }
+            return images;
         }
         private void InitializeGameGrid(int cols, int rows)
         {
