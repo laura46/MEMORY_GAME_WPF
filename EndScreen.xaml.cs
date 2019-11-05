95eb2784bd61114a31f0ac6f0eaf3fb5214fa365
Miel <mielfidder@gmail.com>
2019-11-05 09:35:29 +0100

    endscreen leesbaar

--- EndScreen.xaml.cs
+++ EndScreen.xaml.cs
@@ -24,9 +24,22 @@ namespace MemoryProject
         public EndScreen(Player player1, Player player2, TimeSpan time)
         {
             InitializeComponent();
-            player1Frame.Content = player1.Name + " " + player1.Score + " Powerups: 2X: " + player1.Powerups.X2 + " 4X: " + player1.Powerups.X4 + " 6X: " + player1.Powerups.X6;
-            player2Frame.Content = player2.Name + " " + player2.Score + " Powerups: 2X: " + player2.Powerups.X2 + " 4X: " + player2.Powerups.X4 + " 6X: " + player2.Powerups.X6;
-            timerFrame.Content = time.ToString().Split(new char[] {'.'})[0];
+            Label lblPlayer1 = new Label();
+            lblPlayer1.FontSize = 18;
+            lblPlayer1.FontWeight = FontWeights.Bold;
+            lblPlayer1.Content = player1.Name + " " + player1.Score + " \nPowerups: \n2X: " + player1.Powerups.X2 + " \n4X: " + player1.Powerups.X4 + " \n6X: " + player1.Powerups.X6;
+            Label lblPLayer2 = new Label();
+            lblPLayer2.FontSize = 18;
+            lblPLayer2.FontWeight = FontWeights.Bold;
+            lblPLayer2.Content = player2.Name + " " + player2.Score + " \nPowerups: \n2X: " + player2.Powerups.X2 + " \n4X: " + player2.Powerups.X4 + " \n6X: " + player2.Powerups.X6;
+            player1Frame.FontSize = 200;
+            player1Frame.Content = lblPlayer1;
+            player2Frame.Content = lblPLayer2;
+            Label lblTimer = new Label();
+            lblTimer.FontSize = 18;
+            lblTimer.FontWeight = FontWeights.Bold;
+            lblTimer.Content = time.ToString().Split(new char[] { '.' })[0];
+            timerFrame.Content = lblTimer;
         }
 
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
